133fcc7706a0c0a644d7bc7ba6f2da1c66d60e1a
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-05-18 12:07:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn=document.getElementById("btn");
+
+let function recolor(){
+	var select=document.getElementById("colorSelect");
+          select.remove(x.selectedIndex);
+}
+
+btn.addEventListener("click", recolor())
